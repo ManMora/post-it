dd5c982782044bf0d9e03b38d682b8711a96d291
ManMora <manu11724@gmail.com>
2014-10-03 02:11:55 -0500

    Improved UI post it main

--- main.php
+++ main.php
@@ -1,13 +1,16 @@
-<?php 
+<?php
 	include("header.php");
 ?>
-	<div class="outerWrapper">
-		<div class="postIt">
-			<input class="title" maxlength="20" type="text" name="title">
-			<hr/>
-			<textarea class="post"></textarea>
-		</div>
-	</div>
+   <div class="postCollection">
+        <div class="postWrapper">
+            <div class="postIt">
+                <input class="title" maxlength="20" type="text" name="title">
+                <hr/>
+                <textarea class="post"></textarea>
+            </div>
+        </div>
+
+    </div>
 <?php
 	include("footer.php");
 ?>
